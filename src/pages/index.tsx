799e2997b08b0fb8ab34338740f6017986336b8f
Luan Nascimento <luan.nascimento11@outlook.com>
2021-04-05 21:18:17 -0300

    Init challenge

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { GetStaticProps } from 'next';
-
+import Prismic from '@prismicio/client';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,13 +25,26 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <div>
+      <h1>Opa</h1>
+    </div>
+  );
+}
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'posts')],
+    {
+      fetch: ['posts.title', 'posts.content'],
+      pageSize: 20,
+    }
+  );
+  console.log(postsResponse);
 
-//   // TODO
-// };
+  return {
+    props: {},
+  };
+};
