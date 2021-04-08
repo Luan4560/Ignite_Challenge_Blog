d36bb8653cd3b7af0438a36447ac85c8c3539c94
Luan Nascimento <luan.nascimento11@outlook.com>
2021-04-07 23:44:10 -0300

    Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,13 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import style from './header.module.scss';
+
 export default function Header() {
-  // TODO
+  return (
+    <header>
+      <div className={style.container}>
+        <img src="/images/logo.svg" alt="spacetraveling" />
+      </div>
+    </header>
+  );
 }
